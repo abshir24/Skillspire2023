6a3a9c7073ea9a4b5fd5ca957bfae6a9ab143466
abshir24 <abshirm@live.com>
2023-11-11 11:14:33 -0800

    updated fa3 to use session

--- Python/Flask/fa3/app.py
+++ Python/Flask/fa3/app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
-@app.route("/") 
+app.secret_key = 'ThisIsSecret' # you need to set a secret key for security purposes
+
+@app.route("/",  methods = ['GET', 'POST']) 
 def index():
+    if request.method == "POST":
+        session['username'] = request.form["username"]
+        session['email'] = request.form["email"]
+        session['favorite_food'] = request.form['favorite_food']
+        return render_template("index.html")        
+    
     return render_template("index.html")
 
 @app.route('/process-data', methods = ['GET', 'POST'])
@@ -11,7 +19,6 @@ def index():
 def processData():
     return render_template("data.html", userdata = request.form)
 
-# user inputs data => data moves to request.form object on server => data gets sent back to user
 
 
 if __name__ == "__main__":
