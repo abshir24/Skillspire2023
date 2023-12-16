98fa9fb9aee7fe4a116243db0b1b0275bd243944
abshir24 <abshirm@live.com>
2023-12-16 11:01:27 -0800

    added CRUD operations to demo

--- Python/Django/demo/demoapp/urls.py
+++ Python/Django/demo/demoapp/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path(r'',views.home),
+    path(r'register',views.register),
+    path(r'update',views.update),
     path(r'coding-playlist/', views.coding),
     path(r'earth-playlist', views.earth)
 ]
--- Python/Django/demo/demoapp/views.py
+++ Python/Django/demo/demoapp/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse,redirect
+from .models import User
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = User.objects.create(username=username, email = email, password = password)
+
+        return render(request,"formdata.html",context = { "user" : user})
+    else:
+        return redirect('/')
+
+def update(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        
+        user = User.objects.filter(email = email).first()
+
+        if user:
+            user.username = username
+            user.save()
+            return render(request,"formdata.html",context = { "user" : user})
+        else:
+            return HttpResponse('User does not exist')
+    return redirect('/')
 
 def home(request):
-    return render(request,"home.html")
+    return render(request,"index.html")
 
 def coding(request):
     context = {
